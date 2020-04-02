65da7fced20ddf7cf803dabde68ee7314556e567
Tiramisu-llh <422994005@qq.com>
2020-04-02 21:44:20 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <div>
+      <router-link to="/">云班课</router-link>
+      <div>
+        <ul>
+          <router-link to="/" tag="li">教学包</router-link>
+          <router-link to="/" tag="li">库管理</router-link>
+          <router-link to="#" tag="li">关于我们</router-link>
+          <router-link to="/" tag="li">反馈</router-link>
+          <li>
+            <a href="">GitHub地址</a>
+          </li>
+        </ul>
+        <!-- 用户信息等 -->
+        <ul>
+          <li>
+            <router-link to="/">登录</router-link>
+            <a>退出</a>
+          </li>
+          <router-link to="/" tag="li">帮助</router-link>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({}),
+  computed: {
+    // ...mapState({
+    //   userInfo: state => state.user.userInfo
+    // })
+  },
+  methods: {
+    //     logout() {
+    //       this.$store.dispatch("user/logout");
+    //     }
+    //   }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,124 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-row>
+      <el-col :md="7" :sm="10">
+        <img src="@/assets/computing-on-a-chart.png" width="180" />
+      </el-col>
+      <el-col :md="17" :sm="14" class="grid-content">
+        <h2>课程管理系统</h2>
+        <h6>Course Management System</h6>
+        <p>
+          &emsp;课程管理系统就是为课程建立网站，老师可以在其上发布课程大纲、教学计划,
+          以及每堂课的教学内容，布置作业,批改作业，公布学生成绩。
+          课程管理系统往往还为课程提供一些通讯工具，如讨论组、聊天室,
+          支持这门课程师生之间的交流。
+        </p>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8" class="grid-content">
+        <el-card shadow="hover">
+          <img src />
+          <div>
+            <el-popover content="任务分配：系统设计实现">
+              <el-button slot="reference">
+                <h3>Han Qin</h3>
+              </el-button>
+            </el-popover>
+          </div>
+          <p>
+            <i class="el-icon-ice-tea pl-1 pr-1 pointer"></i>
+            <i class="el-icon-chat-line-round pl-1 pr-1 pointer"></i>
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :span="8" class="grid-content">
+        <el-card shadow="hover">
+          <img src />
+          <div>
+            <el-popover content="任务分配：系统BUG测试">
+              <el-button slot="reference">
+                <h3>Xia Yu Long</h3>
+              </el-button>
+            </el-popover>
+          </div>
+          <p>
+            <i class="el-icon-ice-tea pl-1 pr-1 pointer"></i>
+            <i class="el-icon-chat-line-round pl-1 pr-1 pointer"></i>
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :span="8" class="grid-content">
+        <el-card shadow="hover">
+          <img src />
+          <div>
+            <el-popover content="任务分配：系统需求分析">
+              <el-button slot="reference">
+                <h3>Zhang Kai Li</h3>
+              </el-button>
+            </el-popover>
+          </div>
+          <p>
+            <i class="el-icon-ice-tea pl-1 pr-1 pointer"></i>
+            <i class="el-icon-chat-line-round pl-1 pr-1 pointer"></i>
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :span="8" class="grid-content">
+        <el-card shadow="hover">
+          <img src />
+          <div>
+            <el-popover content="任务分配：数据库设计">
+              <el-button slot="reference">
+                <h3>Guo Wang Yu</h3>
+              </el-button>
+            </el-popover>
+          </div>
+          <p>
+            <i class="el-icon-ice-tea pl-1 pr-1 pointer"></i>
+            <i class="el-icon-chat-line-round pl-1 pr-1 pointer"></i>
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :span="8" class="grid-content">
+        <el-card shadow="hover">
+          <img src />
+          <div>
+            <el-popover content="其他小伙伴">
+              <p>其他小伙伴</p>
+              <a href="http://geekjoker.com/">他的作品</a>
+              <el-button slot="reference">
+                <h3>Zheng Quan</h3>
+              </el-button>
+            </el-popover>
+          </div>
+          <p>
+            <i class="el-icon-ice-tea pl-1 pr-1 pointer"></i>
+            <i class="el-icon-chat-line-round pl-1 pr-1 pointer"></i>
+          </p>
+        </el-card>
+      </el-col>
+    </el-row>
+    <div>
+      <h5>Get started with here and build an era of cloud courses</h5>
+      <router-link to="/">Sign Up Free</router-link>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data: () => ({})
+}
+</script>
+
+<style scoped>
+.grid-content {
+  text-align: center;
+  background-color: rgb(44, 143, 121);
+  border-radius: 4px;
+  min-height: 150px;
+  min-width: 100px;
+}
+</style>
